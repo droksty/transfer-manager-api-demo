5a8e0214f5a9e5c06aae6b30c4444c179d442159
droksty <droksty@gmail.com>
2024-03-23 01:00:44 +0200

    Update Client & Transfer

--- src/main/java/io/droksty/transfersdemo/model/Client.java
+++ src/main/java/io/droksty/transfersdemo/model/Client.java
@@ -3,18 +3,24 @@ package io.droksty.transfersdemo.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "Clients")
 @Data
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
     private Integer id;
 
-    @Column(unique = true, nullable = true)
+    @Column(name = "Title", unique = true, nullable = false)
     private String title;
+
 
     // Avoid bidirectional mapping, for now
     /*@OneToMany(mappedBy = "client")
+    @JsonIgnore
     private List<Transfer> transfers = new ArrayList<>();*/
 }
--- src/main/java/io/droksty/transfersdemo/model/Transfer.java
+++ src/main/java/io/droksty/transfersdemo/model/Transfer.java
@@ -1,5 +1,6 @@
 package io.droksty.transfersdemo.model;
 
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -8,32 +9,70 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Entity
-@Table(name = "Transfers", indexes = {
-        @Index(name = "Date_idx", columnList = "pickup_date"),
-})
+@Table(name = "Transfers", indexes = { @Index(name = "IX_PickupDate", columnList = "PickupDate") })
 @Data
 public class Transfer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @Column(name = "Id")
+    private Integer     id;
 
-    @Column(nullable = false) private LocalDate pickupDate;
-    @Column(nullable = false) private LocalTime pickupTime;
-    @Column(nullable = false) private String    passengerName;
-    @Column(nullable = false) private Short     pax;
-    @Column(nullable = false) private Type      type;
-    @Column(nullable = false) private String    transferFrom;
-    @Column(nullable = false) private String    transferTo;
-    @Column(nullable = false) private Float     priceTotal;
-    @Column(nullable = false) private Float     priceNet;
+    @Column(nullable = false, name = "PickupDate")
+    private LocalDate pickupDate;
+
+    @Column(nullable = false, name = "PickupTime")
+    private LocalTime pickupTime;
+
+    @Column(name = "PassengerName")
+    private String passengerName;
+
+    @Column(name = "Pax")
+    private Short pax;
+
+    @Column(name = "Type")
+    @Enumerated
+    private Type type;
+
+    @Column(nullable = false, name = "TransferFrom")
+    private String transferFrom;
+
+    @Column(nullable = false, name = "TransferTo")
+    private String transferTo;
+
+    @Column(name = "PriceTotal")
+    private Float priceTotal;
+
+    @Column(name = "PriceNet")
+    private Float priceNet;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+    @JoinColumn(foreignKey = @ForeignKey(name="Client_Id"))
+    private Client client;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    @JoinColumn(foreignKey = @ForeignKey(name="Client_FK_Id"))
-    private Client client;
+    @JoinColumn(foreignKey = @ForeignKey(name = "Provider_Id"))
+    private Provider provider;
+
+    @Column(name = "ProviderCost")
+    private Float providerCost;
 
 
-    // Υποπεριπτώσεις όπου ένα transfer εκτελείται από περισσότερα από ένα οχήματα / οδηγούς ??
-    /*private List<String> vehicles;
-    private List<String> drivers;*/
+    // Υποπερίπτωση όπου ένα transfer εκτελέστηκε από περισσότερους από έναν Provider ??
+    /*@Column(name = "ProviderPax")
+    private Short       providerPax;*/
+    /*@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(
+            name = "Transfers_Providers",
+            joinColumns = @JoinColumn(
+                    name = "transfer_id",
+                    referencedColumnName = "id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "provider_id",
+                    referencedColumnName = "id"
+            )
+    )
+    private List<Provider> providers = new ArrayList<>();*/
 }
