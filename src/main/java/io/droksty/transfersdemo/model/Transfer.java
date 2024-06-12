4eceeb0793c9bb9ebb37270c57ae6588f3328ab5
droksty <droksty@gmail.com>
2024-06-12 12:33:21 +0300

    Refactor Transfer entity. A simpler, more readable bean, with JavaBean-style getters & setters.  No Lombok dependency.

--- src/main/java/io/droksty/transfersdemo/model/Transfer.java
+++ src/main/java/io/droksty/transfersdemo/model/Transfer.java
@@ -3,75 +3,150 @@ package io.droksty.transfersdemo.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 /**
- * Encapsulates a Transfer. The following rules apply:
+ * Transfer entity bean.
+ * A Transfer must always have non-null values on the following fields:
  * <ul>
- *     <li>
- *         A Transfer cannot have nulls on the following fields:
- *         <ul>
- *             <li>pickupDate</li>
- *             <li>pickupTime</li>
- *             <li>transferFrom</li>
- *             <li>transferTo</li>
- *         </ul>
- *     </li>
- *     <li>A Transfer may have 0 or 1 clients.</li>
- *     <li>A Transfer may have 0 or 1 operators.</li>
- *     <li>A Transfer's client should not be the same as its operator, unless both values are null.</li>
+ *   <li>pickupDate</li>
+ *   <li>pickupTime</li>
+ *   <li>transferFrom</li>
+ *   <li>transferTo</li>
  * </ul>
+ * A Transfer's client should not be the same as its operator, unless both values are null.</li>
  */
 @Entity
 @Table(name = "Transfers", indexes = { @Index(name = "IX_PickupDate", columnList = "PickupDate") })
-@Data
 public class Transfer {
+
+    private Long        id;
+    private LocalDate   pickupDate;
+    private LocalTime   pickupTime;
+    private String      passengerName;
+    private Integer     totalPax;
+    private Type        type;
+    private String      transferFrom;
+    private String      transferTo;
+    private Float       priceTotal;
+    private Float       priceNet;
+    private Associate   client;
+    private Associate   operator;
+    private Float       operatorCost;
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Id")
-    private Long id;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     @Column(nullable = false, name = "PickupDate")
-    private LocalDate pickupDate;
+    public LocalDate getPickupDate() {
+        return pickupDate;
+    }
+    public void setPickupDate(LocalDate pickupDate) {
+        this.pickupDate = pickupDate;
+    }
 
     @Column(nullable = false, name = "PickupTime")
-    private LocalTime pickupTime;
+    public LocalTime getPickupTime() {
+        return pickupTime;
+    }
+    public void setPickupTime(LocalTime pickupTime) {
+        this.pickupTime = pickupTime;
+    }
 
     @Column(name = "PassengerName")
-    private String passengerName;
+    public String getPassengerName() {
+        return passengerName;
+    }
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
 
     @Column(name = "TotalPax")
-    private Integer totalPax;
+    public Integer getTotalPax() {
+        return totalPax;
+    }
+    public void setTotalPax(Integer totalPax) {
+        this.totalPax = totalPax;
+    }
 
     @Column(name = "Type")
     @Enumerated
-    private Type type;
+    public Type getType() {
+        return type;
+    }
+    public void setType(Type type) {
+        this.type = type;
+    }
 
     @Column(nullable = false, name = "TransferFrom")
-    private String transferFrom;
+    public String getTransferFrom() {
+        return transferFrom;
+    }
+    public void setTransferFrom(String transferFrom) {
+        this.transferFrom = transferFrom;
+    }
 
     @Column(nullable = false, name = "TransferTo")
-    private String transferTo;
+    public String getTransferTo() {
+        return transferTo;
+    }
+    public void setTransferTo(String transferTo) {
+        this.transferTo = transferTo;
+    }
 
     @Column(name = "PriceTotal")
-    private Float priceTotal;
+    public Float getPriceTotal() {
+        return priceTotal;
+    }
+    public void setPriceTotal(Float priceTotal) {
+        this.priceTotal = priceTotal;
+    }
 
     @Column(name = "PriceNet")
-    private Float priceNet;
+    public Float getPriceNet() {
+        return priceNet;
+    }
+    public void setPriceNet(Float priceNet) {
+        this.priceNet = priceNet;
+    }
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
     @JoinColumn(foreignKey = @ForeignKey(name="Client_Id"))
-    private Associate client;
+    public Associate getClient() {
+        return client;
+    }
+    public void setClient(Associate client) {
+        this.client = client;
+    }
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     @JoinColumn(foreignKey = @ForeignKey(name = "Operator_Id"))
-    private Associate operator;
+    public Associate getOperator() {
+        return operator;
+    }
+    public void setOperator(Associate operator) {
+        this.operator = operator;
+    }
 
     @Column(name = "OperatorCost")
-    private Float operatorCost;
+    public Float getOperatorCost() {
+        return operatorCost;
+    }
+    public void setOperatorCost(Float operatorCost) {
+        this.operatorCost = operatorCost;
+    }
 }
+
+
