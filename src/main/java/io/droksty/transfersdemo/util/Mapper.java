51c716e6c223ed1dee9a33e601ff616ea81e68d9
droksty <droksty@gmail.com>
2024-08-07 19:53:58 +0300

    Added the Mapper utility class

---
+++ src/main/java/io/droksty/transfersdemo/util/Mapper.java
@@ -0,0 +1,47 @@
+package io.droksty.transfersdemo.util;
+
+import io.droksty.transfersdemo.dto.TransferDTO;
+import io.droksty.transfersdemo.model.Transfer;
+
+public final class Mapper {
+
+    // No instances of this class should be available
+    private Mapper() {}
+
+
+    // Utility methods
+    public static Transfer newTransferFrom(TransferDTO transferDTO) {
+        return new Transfer(
+                transferDTO.getPickupDate(),
+                transferDTO.getPickupTime(),
+                transferDTO.getPassengerName(),
+                transferDTO.getTotalPax(),
+                transferDTO.getType(),
+                transferDTO.getTransferFrom(),
+                transferDTO.getTransferTo(),
+                transferDTO.getPriceTotal(),
+                transferDTO.getPriceNet(),
+                transferDTO.getClient(),
+                transferDTO.getOperator(),
+                transferDTO.getOperatorCost()
+        );
+    }
+
+    public static TransferDTO newTransferDTOFrom(Transfer transfer) {
+        return new TransferDTO(
+                transfer.getId(),
+                transfer.getPickupDate(),
+                transfer.getPickupTime(),
+                transfer.getPassengerName(),
+                transfer.getTotalPax(),
+                transfer.getType(),
+                transfer.getTransferFrom(),
+                transfer.getTransferTo(),
+                transfer.getPriceTotal(),
+                transfer.getPriceNet(),
+                transfer.getClient(),
+                transfer.getOperator(),
+                transfer.getOperatorCost()
+        );
+    }
+}
