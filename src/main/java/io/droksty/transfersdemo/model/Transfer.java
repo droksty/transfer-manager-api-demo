7581f8783434bf19c6aac07708b1b583cafd7510
droksty <droksty@gmail.com>
2024-12-26 13:38:38 +0200

    Removed final modifier from Transfer @Entity class

--- src/main/java/io/droksty/transfersdemo/model/Transfer.java
+++ src/main/java/io/droksty/transfersdemo/model/Transfer.java
@@ -20,7 +20,7 @@ import java.time.LocalTime;
  */
 @Entity
 @Table(name = "Transfers", indexes = { @Index(name = "IX_PickupDate", columnList = "PickupDate") })
-public final class Transfer {
+public class Transfer {
 
     private Long        id;
     private LocalDate   pickupDate;
